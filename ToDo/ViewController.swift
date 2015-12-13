15d1876739a2e94a3bb507a096216adf9c649024
Harriet Craven <harrietc52@hotmail.co.uk>
2015-12-13 19:51:07 +0000

    todo textbox and save button

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toDoTextField: UITextField!
+    
+    @IBAction func saveButtonTapped(sender: AnyObject) {
+        
+        let toDoText = toDoTextField.text
+        
+        addStringToArray(toDoText!)
+        
+        print(toDoArray)
+    }
+    
+    func addStringToArray(toDoString: String){
+        toDoArray.append(toDoString)
+    }
+    
+    var toDoArray : [String] = []
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
